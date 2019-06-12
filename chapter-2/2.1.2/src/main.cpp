7186d667a3c81a26ec3811372d010e457b63a38f
Ahmed Fawzy Elaraby <ahmedfawzyelaraby@gmail.com>
2019-06-12 07:00:47 +0200

    [feat]: solve singly linked list searching example

---
+++ chapter-2/2.1.2/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdlib.h>
+
+struct singlyLinkesListNode{
+	int nodeValue;
+	singlyLinkesListNode* nextNode;
+};
+
+int main(int argc, char** argv){
+	if(argc <= 1){
+		std::cout << "Invalid input" << std::endl;
+	}
+
+	singlyLinkesListNode* head = NULL;
+	singlyLinkesListNode* tail = NULL;
+
+	for(int iterator = 1; iterator < (argc - 1); iterator++){
+		if(head == NULL){
+			head = new singlyLinkesListNode();
+			head->nodeValue = atoi(argv[iterator]);
+			head->nextNode = NULL;
+			tail = head;
+		}else{
+			tail->nextNode = new singlyLinkesListNode();
+			tail = tail->nextNode;
+			tail->nodeValue = atoi(argv[iterator]);
+			tail->nextNode = NULL;
+		}
+	}
+
+	std::cout << "Head Value: " << head->nodeValue << std::endl;
+	std::cout << "Tail Value: " << tail->nodeValue << std::endl;
+
+	singlyLinkesListNode* currentNode = head;
+	while((currentNode != NULL) && (currentNode->nodeValue != atoi(argv[argc - 1]))){
+		currentNode = currentNode->nextNode;
+	}
+
+	if(currentNode == NULL){
+		std::cout << "Item not found" << std::endl;
+	}else{
+		std::cout << "Item found" << std::endl;
+	}
+
+	return 0;
+}
