ebcf76dc1da05ce20830225d681778738ed0a237
Ahmed Fawzy Elaraby <ahmedfawzyelaraby@gmail.com>
2019-06-14 14:03:23 +0200

    [feat]: solve singly linked list forward traversal example

---
+++ chapter-2/2.1.4/src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+
+struct singlyLinkesListNode{
+	int nodeValue;
+	singlyLinkesListNode* nextNode;
+};
+
+int main(int argc, char** argv){
+	if(argc <= 1){
+		std::cout << "Invalid input" << std::endl;
+	}
+
+	singlyLinkesListNode* head = NULL;
+	singlyLinkesListNode* tail = NULL;
+
+	for(int iterator = 1; iterator < (argc); iterator++){
+		if(head == NULL){
+			head = new singlyLinkesListNode();
+			head->nodeValue = atoi(argv[iterator]);
+			head->nextNode = NULL;
+			tail = head;
+		}else{
+			tail->nextNode = new singlyLinkesListNode();
+			tail = tail->nextNode;
+			tail->nodeValue = atoi(argv[iterator]);
+			tail->nextNode = NULL;
+		}
+	}
+
+	std::cout << "Head Value: " << head->nodeValue << std::endl;
+	std::cout << "Tail Value: " << tail->nodeValue << std::endl;
+
+	singlyLinkesListNode* currentNode = head;
+	while(currentNode != NULL){
+		std::cout << currentNode->nodeValue << std::endl;
+		currentNode = currentNode->nextNode;
+	}
+
+	return 0;
+}
