cdd6c91460b3bf2ad60099e762836a43d6dc5afa
Ahmed Fawzy Elaraby <ahmedfawzyelaraby@gmail.com>
2019-06-19 20:30:57 +0200

    [feat]: solve binary search tree search example

---
+++ chapter-3/3.2/src/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stdlib.h>
+
+struct binarySearchTreeNode{
+	int nodeValue;
+	binarySearchTreeNode* leftNode;
+	binarySearchTreeNode* rightNode;
+};
+
+void insertNode(binarySearchTreeNode* currentNode, binarySearchTreeNode* nodeToAdd){
+	if(nodeToAdd->nodeValue < currentNode->nodeValue){
+		if(currentNode->leftNode == NULL){
+			currentNode->leftNode = nodeToAdd;
+		}else{
+			insertNode(currentNode->leftNode, nodeToAdd);
+		}
+	}else{
+		if(currentNode->rightNode == NULL){
+			currentNode->rightNode = nodeToAdd;
+		}else{
+			insertNode(currentNode->rightNode, nodeToAdd);
+		}
+	}
+}
+
+bool findNode(binarySearchTreeNode* currentNode, int searchValue){
+	if(currentNode == NULL){
+		return false;
+	}
+
+	if(currentNode->nodeValue == searchValue){
+		return true;
+	}else if(currentNode->nodeValue > searchValue){
+		return findNode(currentNode->leftNode, searchValue);
+	}else{
+		return findNode(currentNode->rightNode, searchValue);
+	}
+}
+
+int main(int argc, char** argv){
+	if(argc <= 1){
+		std::cout << "Invalid input" << std::endl;
+	}
+
+	binarySearchTreeNode* root = NULL;
+	for(int iterator = 1; iterator < (argc - 1); iterator++){
+		if(root == NULL){
+			root = new binarySearchTreeNode();
+			root->nodeValue = atoi(argv[iterator]);
+			root->leftNode = NULL;
+			root->rightNode = NULL;
+		}else{
+			binarySearchTreeNode* nodeToAdd = new binarySearchTreeNode();
+			nodeToAdd->nodeValue = atoi(argv[iterator]);
+			insertNode(root, nodeToAdd);
+		}
+	}
+
+	std::cout << "Element is found status: " << findNode(root, atoi(argv[argc - 1])) << std::endl;
+
+	return 0;
+}
